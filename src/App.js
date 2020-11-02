78ccae09897eb17956cdce4073c0feeb70eb3f4d
Karol Gebarowski <karol.gebarowski@gmail.com>
2020-11-02 19:56:06 +0100

    install bulma and sass, remove unnecessary CRA files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+      <div className="section is-medium has-background-success">
+      <div className="container">
+
+        <h1 className="title is-1 has-text-light ">BULMA srapi</h1>
+        <p id="testing">
+          let's begin
+
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      </div>
     </div>
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "bulma";
+import "./index.scss"
+
+reportWebVitals(console.log)
 
 ReactDOM.render(
   <React.StrictMode>
