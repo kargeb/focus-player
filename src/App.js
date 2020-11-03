9e984e90ab8b451ed675cfee769e08d83089a72f
Karol Gebarowski <karol.gebarowski@gmail.com>
2020-11-03 09:23:20 +0100

    install and config eslint and prettier

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-
 function App() {
   return (
     <div>
       <div className="section is-medium has-background-success">
-      <div className="container">
-
-        <h1 className="title is-1 has-text-light ">BULMA srapi</h1>
-        <p id="testing">
-          let's begin
-
-        </p>
-      </div>
+        <div className="container">
+          <h1 className="title is-1 has-text-light ">BULMA player</h1>
+          <p id="testing">lets begin</p>
+        </div>
       </div>
     </div>
-
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import "bulma";
-import "./index.scss"
-
-reportWebVitals(console.log)
+import 'bulma';
+import './index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
