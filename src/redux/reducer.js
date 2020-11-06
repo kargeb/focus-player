bd7f067af3fdd0ff7123e72f72bf98dc60a40abe
Karol Gebarowski <karol.gebarowski@gmail.com>
2020-11-06 13:57:14 +0100

    install redux-thunk, prepare actions and reducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,11 +1,18 @@
 const TEST = 'TEST';
+const FETCH_FILMS_REQUESTED = 'FETCH_FILMS_REQUESTED';
+const FETCH_FILMS_SUCCEEDED = 'FETCH_FILMS_SUCCEEDED';
+const FETCH_FILMS_FAILED = 'FETCH_FILMS_FAILED';
 
-export const test = () => ({
-  type: TEST,
-});
+export const fetchRequested = () => ({ type: FETCH_FILMS_REQUESTED });
+export const fetchFailed = () => ({ type: FETCH_FILMS_FAILED });
+export const fetchSucceeded = () => ({ type: FETCH_FILMS_SUCCEEDED, payload: films });
+export const test = () => ({ type: TEST });
 
 const INITIAL_STATE = {
+  films: [],
   testValue: 23,
+  isLoading: false,
+  isError: false,
 };
 
 export const reducer = (state = INITIAL_STATE, action) => {
@@ -15,6 +22,25 @@ export const reducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         testValue: state.testValue + 5,
+      };
+    case FETCH_FILMS_REQUESTED:
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case FETCH_FILMS_FAILED:
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    case FETCH_FILMS_SUCCEEDED:
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        films: [...action.payload],
       };
     default:
       return state;
