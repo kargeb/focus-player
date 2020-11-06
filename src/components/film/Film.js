1fcf10cc190b2f6847d518b05808244f710d0ae4
Karol Gebarowski <karol.gebarowski@gmail.com>
2020-11-06 14:46:22 +0100

    clean consologs and apiHelper file

--- src/components/film/Film.js
+++ src/components/film/Film.js
@@ -1,12 +1,10 @@
 import ReactPlayer from 'react-player/youtube';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { test } from '../../redux/reducer';
 
 const Film = () => {
   const { id } = useParams();
   const film = useSelector((state) => state.films.find((item) => item.id === id));
-  console.log('ZNALAZLEM FILM: ', film);
 
   return (
     <section className="section">
@@ -14,9 +12,7 @@ const Film = () => {
         <h4>URL id: {id}</h4>
         <h4>FILM id: {film.id}</h4>
         <h2 className="subtitle is-4 pl-6">{film.title}:</h2>
-
         <p>{film.description}</p>
-
         <ReactPlayer url={film.video_url} controls />
       </div>
     </section>
