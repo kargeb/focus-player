8e9d9f6295c5bd515c6575269232bf7601ef7565
Karol Gebarowski <karol.gebarowski@gmail.com>
2020-11-07 12:37:12 +0100

    get api data in index.js file

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import { Provider, useDispatch } from 'react-redux';
 
 import store from './redux/store';
+import { fetchFilms } from './redux/reducer';
 import Dashboard from './components/dashboard/Dashboard';
 import Navbar from './components/navbar/Navbar';
 import Films from './components/films/Films';
 import Film from './components/film/Film';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchFilms());
+  }, []);
+
   return (
-    <Provider store={store}>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Dashboard />
-          </Route>
-          <Route exact path="/films">
-            <Films />
-          </Route>
-          <Route path="/films/:id">
-            <Film />
-          </Route>
-        </Switch>
-      </Router>
-    </Provider>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Dashboard />
+        </Route>
+        <Route exact path="/films">
+          <Films />
+        </Route>
+        <Route path="/films/:id">
+          <Film />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
