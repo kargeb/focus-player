981243ee7e5d1d69643dfcffc70e7eb90dc00631
Karol Gebarowski <karol.gebarowski@gmail.com>
2021-01-26 14:21:26 +0100

    align filmContainer buttons

--- src/components/films/film/filmContainer/NewFilmContainer.js
+++ src/components/films/film/filmContainer/NewFilmContainer.js
@@ -30,15 +30,51 @@ const FilmContainer = () => {
           </div>
 
           {/* <div className="column  featured-content va"> */}
-          <div className="column is-4-widescreen is-offset-1-widescreen is-8-tablet is-offset-2-tablet featured-content va">
+          <div className="column is-4-widescreen is-offset-0-widescreen is-8-tablet is-offset-2-tablet featured-content va">
             <div>
               {/* <h3 className="heading post-category">Category Name</h3> */}
               <h1 className="title post-title">{currentFilm.title}</h1>
               <p className="post-excerpt">{currentFilm.description}</p>
               <br />
-              <a href="https://ghost.io" className="button is-primary">
+              {/* <a href="https://ghost.io" className="button is-primary">
                 Read More
-              </a>
+              </a> */}
+              <div className="is-flex is-justify-content-space-between ">
+                <span className="icon-text has-text-primary is-flex is-align-items-center">
+                  <span className="icon is-medium">
+                    <i className="far fa-square fa-lg" />
+                  </span>
+                  <span>Watched</span>
+                </span>
+
+                <div>
+                  <button
+                    type="button"
+                    className="button is-primary _without-border is-outlined mr-4 is-hidden-mobile"
+                    // onClick={() => dispatch(toggleEditFilmMode())}
+                  >
+                    <span className="icon  is-small px-5">
+                      <i className="fas fa-edit" />
+                    </span>
+                  </button>
+                  <button
+                    type="button"
+                    className="button is-primary _without-border is-outlined is-hidden-mobile"
+                    // onClick={() => dispatch(toggleEditFilmMode())}
+                  >
+                    <span className="icon  is-small ">
+                      <i className="fas fa-trash-alt" />
+                    </span>
+                  </button>
+                </div>
+              </div>
+
+              <button type="button" className="button is-primary">
+                edit
+              </button>
+              <button type="button" className="button is-primary">
+                remove
+              </button>
             </div>
           </div>
         </article>
