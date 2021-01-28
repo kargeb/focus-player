5963e442832b89bba2088a28105f8a3f33ff6219
Karol Gebarowski <karol.gebarowski@gmail.com>
2021-01-28 16:49:01 +0100

    sort filmsList by title localCompare

--- src/components/films/filmsList/FilmsList.js
+++ src/components/films/filmsList/FilmsList.js
@@ -3,18 +3,20 @@ import FilmItem from './filmItem/FilmItem';
 const FilmsList = ({ films, columns = 3 }) => (
   <div>
     <ul className="columns is-multiline">
-      {films.map((item) => (
-        <li key={item.id} className={`column is-${columns}`}>
-          <FilmItem
-            description={item.description}
-            title={item.title}
-            url={item.video_url}
-            id={item.id}
-            timestamp={item.timestamp}
-            watched={item.watched}
-          />
-        </li>
-      ))}
+      {films
+        .sort((a, b) => a.title.localeCompare(b.title))
+        .map((item) => (
+          <li key={item.id} className={`column is-${columns}`}>
+            <FilmItem
+              description={item.description}
+              title={item.title}
+              url={item.video_url}
+              id={item.id}
+              timestamp={item.timestamp}
+              watched={item.watched}
+            />
+          </li>
+        ))}
     </ul>
   </div>
 );
